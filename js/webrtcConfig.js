a0a436fb3ebbdb6d3eb20528c97348ff8572076e
A. Benjamins <deraikoben@gmail.com>
2018-06-14 17:07:50 +0200

    selectorzzz

--- js/webrtcConfig.js
+++ js/webrtcConfig.js
@@ -1,81 +1,116 @@
-var connection = new RTCMultiConnection();
-var roomName = prompt("Please enter the Room Name", "Zange");
+// get references to select list and display text box
+var sel = document.getElementById('scripts');
+var el = document.getElementById('display');
+var connectionChannel = "Zange";
 
-// this line is VERY_important
-connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+function getSelectedOption(sel) {
+    var opt;
+    for (var i = 0, len = sel.options.length; i < len; i++) {
+        opt = sel.options[i];
+        if (opt.selected === true) {
+            break;
+        }
+    }
+    return opt;
+}
+
+// assign onclick handlers to the buttons
+document.getElementById('showVal').onclick = function () {
+    /*var oldVideos = document.getElementsByTagName('video');
+    console.log(oldVideos);
+    if (oldVideos.length > 0) {
+        for (var videox of oldVideos) {
+
+            videox.parentNode.removeChild(videox);
+
+        }
+    }*/
+    connectionChannel = sel.value;
+    initWebRTC();
+    document.getElementById('showVal').disabled = true;
+
+}
 
 
-var desktopConstraints =
-    connection.mediaConstraints = {
+function initWebRTC() {
+    var connection = new RTCMultiConnection();
+    // this line is VERY_important
+    connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
-        video: {
 
-            width: 800,
-            height: 600,
-            frameRate: 15,
+    var desktopConstraints =
+        connection.mediaConstraints = {
 
-        },
+            video: {
 
-        audio: true
-    };
+                width: 800,
+                height: 600,
+                frameRate: 15,
+
+            },
+
+            audio: true
+        };
 
 //constraints for mobile browser
-var mobileConstraints =
-    {
+    var mobileConstraints =
+        {
 
-        video: {
+            video: {
 
-            width: 480,
-            height: 320,
+                width: 480,
+                height: 320,
 
 
-        },
+            },
 
-        audio: true
-    }
+            audio: true
+        }
 
 
 //if a user is using a mobile browser
-if (/Android|iPhone|iPad/i.test(navigator.userAgent)) {
-    var constraints = mobileConstraints;
-} else {
-    var constraints = desktopConstraints;
+    if (/Android|iPhone|iPad/i.test(navigator.userAgent)) {
+        var constraints = mobileConstraints;
+    } else {
+        var constraints = desktopConstraints;
+    }
+
+    function hasUserMedia() {
+        //check if the browser supports the WebRTC
+        return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia);
+    }
+
+    if (hasUserMedia()) {
+
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia;
+
+        //enabling video and audio channels
+        navigator.getUserMedia(constraints, function (stream) {
+            //var video = document.querySelector('video');
+
+            //inserting our stream to the video tag
+            // video.src = window.URL.createObjectURL(stream);
+
+            // if you want audio+video conferencing
+
+
+            connection.openOrJoin(connectionChannel);
+
+            /*connection.sdpConstraints.mandatory = {
+                OfferToReceiveAudio: true,
+                OfferToReceiveVideo: true,
+
+            };*/
+
+
+        }, function (err) {
+        });
+    } else {
+        alert("WebRTC is not supported");
+    }
+
+    console.log("show  " + JSON.stringify(constraints));
+
 }
-
-function hasUserMedia() {
-    //check if the browser supports the WebRTC
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia);
-}
-
-if (hasUserMedia()) {
-
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia;
-
-    //enabling video and audio channels
-    navigator.getUserMedia(constraints, function (stream) {
-        var video = document.querySelector('video');
-
-        //inserting our stream to the video tag
-        video.src = window.URL.createObjectURL(stream);
-
-        // if you want audio+video conferencing
-
-
-        connection.openOrJoin(roomName);
-
-        /*connection.sdpConstraints.mandatory = {
-            OfferToReceiveAudio: true,
-            OfferToReceiveVideo: true,
-
-        };*/
-
-
-    }, function (err) {
-    });
-} else {
-    alert("WebRTC is not supported");
-}
-
-console.log("show  " + JSON.stringify(constraints));
